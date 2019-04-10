054e1a7a0be5a0de7a5cf23cc5b95f3d253fc924
Unknown <renatassmendes@hotmail.com>
2019-04-10 17:46:23 -0300

    Salvando mensagens recebidas em dicionario no server

--- server.py
+++ server.py
@@ -6,8 +6,10 @@ s = socket(AF_INET, SOCK_STREAM)
 s.bind((HOST, PORT))  #-
 s.listen(1)           #-
 
-#dict = {}
+# Dicionario em que o grupo é a chave. Seu valor é uma lista de tuplas. Cada tupla é composta do nome de quem enviou a mensagem e do valor da mensagem.
+dicionario = {}
 
+# Definição do padrão da expressão regular
 padrao = '\(\"(.*)\"\,\"(.*)\"\,\"(.*)\"\)'
 
 (conn, addr) = s.accept()  # returns new socket and addr. client 
@@ -15,12 +17,21 @@ while True:                # forever
 	data = conn.recv(1024)   # receive data from client
 
 	if not data: break       # stop if client stopped
-  #if data[1] not in dict.keys():
-  	#dict[data[1]] = (data[0], data[2])
-  #print("grupo : ", dict[data[1]].key(), dict[data[1]][0], " disse ", dict[data[1]][2])
+
+	# Separando os valores 1 - nome de quem enviou a mensagem, 2 - nome do grupo, 3 - mensagem enviada
 	m = re.search(padrao,data.decode())
 
-	print(m.groups())
+	# Se o grupo é novo
+	if m.group(2) not in dicionario.keys():
+		dicionario[m.group(2)] = []
+
+	# Adicionando nova mensagem ao grupo
+	dicionario[m.group(2)].append((m.group(1), m.group(3)))
+
+	print("grupo : ", m.group(2), dicionario[m.group(2)][-1][0], " disse ", dicionario[m.group(2)][-1][1])
+	
+
+	# print(m.groups())
 	conn.send(m.group(1).encode()) # return sent data plus an "*"
 conn.close()               # close the connection
 
